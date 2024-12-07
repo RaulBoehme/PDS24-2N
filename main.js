d86ac48c342482a2a143c948d25adc693dcae1f5
Raul Boehme <raulboehme@gmail.com>
2024-12-06 21:20:16 -0300

    Add files via upload
    atividaded gbdonline

---
+++ main.js
@@ -0,0 +1,96 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+console.log('Die World, and save yourself');
+
+let A = true
+let B = false
+let result = A && B
+console.log(A, '&&', B, '=', result)
+
+let a2 = true
+let b2 = true
+let resultado2 = a2 && b2
+console.log(a2, '&&', b2, '=', resultado2)
+
+let a3 = false
+let b3 = false
+let resultado3 = a3 && b3
+console.log(a3, '&&', b3, '=', resultado3)
+
+
+let C = 1
+let D = 0
+let result2 = C !== D
+console.log(C, '!==', D, '=', result2)
+
+let c2 = 1 
+let d2 = 1 
+let resultado1 = c2 !== d2
+console.log(c2, '!==', d2, '=', resultado1)
+
+let c3 = 0
+let d3 = 0
+let result4 = c3 !== d3 
+console.log(c3, '!==', d3, '=', result4)
+
+let E = true
+let F = false
+let result3 = E == F
+console.log(E, '==', F, '=', result3)
+
+let e2 = true
+let f2 = true
+let resultado = e2 == f2
+console.log(e2, '==', f2, '=', resultado)
+
+let e3 = false
+let f3 = false
+let resultado4 = e3 == f3 
+console.log(e3, '==', f3, '=', resultado4 )
+
+let G = 4 
+let H = 8 
+let resultado5 = G < H 
+console.log(G, '<', H, '=', resultado5)
+
+let g2 = 3 
+let h2 = 0 
+let resultado6 = g2 < h2 
+console.log(g2, '<', h2, '=', resultado6)
+
+let g3 = 5 
+let h3 = 5 
+let resultado7 = g3 < h3
+console.log(g3, '<', h3, '=', resultado7)
+
+let I = 4 
+let J = 8 
+let resultado8 = I > J 
+console.log(G, '>', H, '=', resultado8)
+
+let i2 = 3 
+let j2 = 0 
+let resultado9 = i2 > j2 
+console.log(i2, '>', j2, '=', resultado9)
+
+let i3 = 5 
+let j3 = 5 
+let resultado10 = i3 > j3
+console.log(i3, '>', j3, '=', resultado10)
+
+
+let idade = 19
+
+let status = idade >= 10 ? 'adulto' : 'menor de idade'
+
+console.log(status);
+
+let palavra1 = 'alfa'
+let palavra2 = 'beto'
+let resultadoP= palavra1 + palavra2
+console.log(resultadoP)
